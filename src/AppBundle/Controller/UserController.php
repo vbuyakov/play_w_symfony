f5e0e8eb4e0cb300fdb448ea96d876d7bd9acd2a
Viktor Buyakov <viktorbuyakov@cloud.upwork.com>
2017-09-10 22:02:40 +0300

    inititaly project with REST example

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viktor
+ * Date: 25.08.17
+ * Time: 0:19
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
+
+class UserController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/user")
+     */
+    public function getAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+        if ($restresult === null) {
+            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+        }
+        return $restresult;
+    }
+
+}
